5b50ffe8834fd35f756ce1ef64b4a95f1b6c41aa
Irving E Rabel <rabel606@gmail.com>
2018-09-25 21:24:45 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 your_hash={ "my_name"=> "Irving"}
-  
+  your_hash
 end
 
 
@@ -37,8 +37,8 @@ shipping_manifest["muskets"] =2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-shipping_manifest
+shipping_manifest["gun powder"]=4
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
